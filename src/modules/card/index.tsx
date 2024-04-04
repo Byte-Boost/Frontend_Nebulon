8838f712d14ac16d4401084a3aa7c3bbfd613a76
Arthur Sousa <112907764+Meowo2@users.noreply.github.com>
2024-04-04 08:35:36 -0300

    [feat]: start of Card module

---
+++ src/modules/card/index.tsx
@@ -0,0 +1,13 @@
+
+const Card = () => {
+    return(
+    <>
+        <div className="card flex basis-1/2 m-4 drop-shadow-md place-content-center">
+            <div className="card--content shrink m-2">
+                
+            </div>
+        </div>
+    </>
+    )
+}
+export default Card
