452e20ed93f90aad398bc1811f1ed073ea9ac13a
Markos Nunes <105757405+MarkVN2@users.noreply.github.com>
2024-04-06 15:42:09 -0300

    [style]: fixed responsivity and hid searchbar
    - searchbar is now hidden since it doesn't have a functionality yet.

--- src/modules/navbar/index.tsx
+++ src/modules/navbar/index.tsx
@@ -3,17 +3,15 @@ import NBSearchbar from "../nb-searchbox";
 
 const Navbar = () => {
     return(
-        <nav className="navbar navbar--fixed-top max-sm:grid-cols-1 max-sm:grid">
-            <div className="navbar__inner ">
-                <div className="navbar__items navbar__items--left ">
+        <nav className="navbar navbar--fixed-top max-sm:grid-cols-1 max-sm:grid  ">
+            <div className="navbar__inner">
+                <div className="navbar__items navbar__items--left max-sm:justify-center">
                   <p className="font-thin text-white text-opacity-75 select-none ">Nebulon-v0.0.0a</p>
                 </div>
-                <div className="navbar__items navbar__items--right">
-                    <div className="bar-search max-sm:invisible max-sm:w-0 max-sm:h-0"/>
-                    <div>
+                <div className=" navbar__items navbar__items--right max-sm:justify-center ">
+                    <div className=" bar-search max-sm:invisible max-sm:w-0 max-sm:h-0 max-sm:absolute"/>
                     <NBSearchbar/>
-                    </div>
-                </div>
+                </div>  
             </div>
         </nav>
     );
--- src/modules/nb-searchbox/index.tsx
+++ src/modules/nb-searchbox/index.tsx
@@ -1,13 +1,13 @@
 
 const NBSearchbar = () =>{
     return(
-        <search>
-            <div className="container--searchbar">
+        <search className=" invisible max-sm:w-0 max-sm:h-0"> {/* This is a searchbar component that is hidden for now */ }
+            <div className="container--searchbar max-sm:p-0 max-sm align-middle ">
                 <input 
                 type="search" 
                 placeholder="Search"
                 id="search" 
-                className="nb-searchbar"
+                className="nb-searchbar "
                 name="q"
                 />
             </div>
