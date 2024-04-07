88b665f47ba50bce7a18b1c189dc75f843505c3c
Markos Nunes <105757405+MarkVN2@users.noreply.github.com>
2024-04-07 20:57:36 -0300

    [feat]: start of content component

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,17 @@
 import '@/app/globals.css'
 import Navbar from '@/modules/navbar';
 import Sidebar from '@/modules/sidebar';
+import Content from '@/modules/content';
 export default function Home() {
   return (
     <main>
         <Navbar/>
+        <section className='grid grid-flow-col-dense'> {/* Fix later now i am tired */}
           <Sidebar/>
+          <Content header='/Home'>
+            <p>a</p>
+          </Content>
+        </section>
     </main>
   );
 }
