ef4721ddc2dd33a25d80af6430820506db911bad
Vinicius Forcato <76211125+nininhosam@users.noreply.github.com>
2024-04-11 20:15:23 -0300

    [style]: changed add page
    and removed useless/bad CSS

--- src/pages/add.tsx
+++ src/pages/add.tsx
@@ -70,13 +70,17 @@ export default function Products() {
         <Sidebar/>
         <ContentArea>
         <div className='w-full p-14'>
-            <div className='container p-14'>
-                <div className='flex items-center flex-col justify-center'>
-                    <label className='label-p m-6' htmlFor='file'>Selecionar arquivo</label>
-                    <input className='inp-p' type="file" name="file" id='file' required accept='.xlsx' onChange={handleChange}/>
-                    <button className='btn-p-send' onClick={onSend}>
-                        Enviar
-                    </button>
+            <div className='bg-slate-100 shadow-2xl rounded-sm py-6 px-6'>
+              <h1 className='text-3xl mb-4'>Extrair informações do excel</h1>
+              Selecionar arquivo
+              <div className='bg-slate-300 border-dashed border-2 border-black h-56 flex items-center flex-col justify-center cursor-pointer'>
+                <p>Arraste e solte o arquivo aqui</p>
+                <p>ou</p>
+                <label className='mt-12 bg-purple-500 hover:bg-purple-600 text-white p-2 cursor-pointer' htmlFor='file'>Escolha um arquivo</label>
+                <input className='hidden' type="file" name="file" id='file' required accept='.xlsx' onChange={handleChange}/>
+              </div>
+              <div className="text-right">
+                <button className='mt-4' onClick={onSend}>Upload</button>
                 </div>
             </div>
           </div>
