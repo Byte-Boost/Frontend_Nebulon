cba76162873560df4ea400586d9bee5f0d5de8ab
Markos Nunes <105757405+MarkVN2@users.noreply.github.com>
2024-05-21 15:59:47 -0300

    [feat]: authentication for routes
    working as intended for now ;)

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,25 +1,40 @@
 import { NextResponse } from 'next/server'
 import type { NextRequest } from 'next/server'
 import { isAuthenticated, isAuthenticatedAsAdmin } from './scripts/utils/auth'
-import Cookies from 'js-cookie';
 
 const protectedRoutes = ["/home","/adicionar/cliente","/adicionar/comissao","/adicionar/produtos","/dashboard/comissao","/dashboard/produto","/dashboard/cliente"]
-const adminProtectedRoutes = ["/adm/usuarios","/adm/usuarios/","/adm/usuarios/criar","/adm/usuarios/editar","/adm/usuarios/deletar","/adm/usuarios/visualizar"]
+const adminProtectedRoutes = ["/adm/usuarios"]
 
-export default function middleware(req: NextRequest) {
-  const token = Cookies.get('token');
+export default function middleware(req: NextRequest, res : NextResponse) {
 
-  if (req.nextUrl.pathname == '/exit') {
+  if (req.nextUrl.pathname == '/') {
     const response = NextResponse.rewrite(new URL('/', req.nextUrl.origin))
     response.cookies.set('token', '', { maxAge: -1 })
     return response
   }
-  if (!isAuthenticated(token) && protectedRoutes.includes(req.nextUrl.pathname)) {
+
+  // console.log('Is authenticated :', isAuthenticated(req));
+  // console.log('Protected routes:', protectedRoutes )
+  // console.log('Requested path:', req.nextUrl.pathname);
+
+  if (!isAuthenticated(req) && protectedRoutes.includes(req.nextUrl.pathname)) {
+    console.log('Not authenticated for protected route');
     const absoluteURL = new URL("/", req.nextUrl.origin)
+    console.log('Redirecting to:', absoluteURL.toString());
     return NextResponse.redirect(absoluteURL.toString())
   }
-  if (!isAuthenticatedAsAdmin(token) && adminProtectedRoutes.includes(req.nextUrl.pathname)){
-    const absoluteURL = new URL("/",req.nextUrl.origin)
-    return NextResponse.redirect(absoluteURL.toString())
+  // console.log('Is authenticated as admin:', isAuthenticatedAsAdmin(req));
+  // console.log('Admin protected routes:', adminProtectedRoutes);
+  // console.log('Requested path:', req.nextUrl.pathname);
+  
+  if (!isAuthenticatedAsAdmin(req) && adminProtectedRoutes.includes(req.nextUrl.pathname)){
+      console.log('Not authenticated as admin for protected route');
+      const absoluteURL  = new URL("/",req.nextUrl.origin)
+      if (isAuthenticated(req)){
+        absoluteURL.pathname = '/home'
+      }
+      console.log('Redirecting to:', absoluteURL.toString());
+      return NextResponse.redirect(absoluteURL.toString())
   }
+  return NextResponse.next();
 }
--- src/scripts/utils/auth.ts
+++ src/scripts/utils/auth.ts
@@ -1,23 +1,24 @@
+import { JwtPayload,  jwtDecode } from 'jwt-decode';
+import nextCookies from 'next-cookies';
+import { NextRequest } from 'next/server';
 
-import { jwtDecode } from 'jwt-decode';
-import { JwtPayload } from 'jwt-decode'; 
 interface MyJwtPayload extends JwtPayload {
   admin: boolean; 
 }
 
-export const isAuthenticated = (token : any) : boolean =>{
-    if (token) {
-      return true;
-    }
-    return false;
-}   
-export const isAuthenticatedAsAdmin = (token : any) : boolean =>{
+
+export const isAuthenticated = (req: NextRequest) => {
+    const headers = Object.fromEntries(req.headers.entries());
+    const { token } = nextCookies({ req: { headers } });
+    return token ? true : false;
+}
+
+export const isAuthenticatedAsAdmin = (req: NextRequest) => {
+    const headers = Object.fromEntries(req.headers.entries());
+    const { token } = nextCookies({ req: {headers} });
     if (token) {
         const decoded = jwtDecode<MyJwtPayload>(token);
-        if (decoded.admin){
-            return true
-        }
-        else  return false;
-    }
-    return false;
+        return decoded.admin ? true : false;
+  }
+  return false;
 }
