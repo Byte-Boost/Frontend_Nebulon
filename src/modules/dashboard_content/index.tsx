0913e6fd86f354c2b505e3eb82be5e545a7ee14e
Markos Nunes <105757405+MarkVN2@users.noreply.github.com>
2024-05-23 22:11:18 -0300

    [feat]: line chart gets db info
    The line chart shows total of sales in R$ in the last 12 months

--- src/modules/dashboard_content/index.tsx
+++ src/modules/dashboard_content/index.tsx
@@ -22,6 +22,7 @@ const DashboardContent = () => {
   const [quantitySellsCurrentMonth, setQuantitySellsCurrentMonth] = useState<number>(0);
   const [totalSellsValue, setTotalSellsValue] = useState<number>(0);
   const [totalComissionValueCurrentMonth, setTotalComissionValueCurrentMonth] = useState<number>(0);
+  const [totalSellsPerMonth, setTotalSellsPerMonth] = useState({});
 
   const [filterLabel, setFilterLabel] = useState<{
     client: string | null,
@@ -33,20 +34,45 @@ const DashboardContent = () => {
     product: null,
   });
 
-  async function getDataForGraph(){
-    
+  async function getDataForGraph() {
+    let now = new Date(Date.now());
+    let promises = [];
+  
+    for(let i = 0; i < 11; i++){
+      let newDate = new Date(now.getFullYear(), now.getMonth() - i, 1);
+      let month_start = `${newDate.getFullYear()}-${newDate.getMonth() + 1}-${1}`
+      let month_end = `${newDate.getFullYear()}-${newDate.getMonth() + 1}-${31}` ;
+  
+      promises.push(
+        instance.get("/commissions/stats", {params :{
+          sale_value_after: month_start,
+          sale_value_before: month_end,
+        }})
+      );
+    }
+  
+    const results = await Promise.all(promises);
+    let newTotalSellsPerMonth : {[key: string]: number} = {...totalSellsPerMonth};
+  
+    results.reverse().forEach((commissions_12m, i) => {
+      let newDate = new Date(now.getFullYear(), now.getMonth() - (results.length - 1 - i), 1);
+      newTotalSellsPerMonth[MonthName(newDate)+'/'+newDate.getFullYear()] = commissions_12m.data.saleValue;
+    });
+  
+    setTotalSellsPerMonth(newTotalSellsPerMonth);
   }
+  
   async function getOverallData(){
     let now = new Date(Date.now());
     let start = new Date(now.setMonth(now.getMonth()));
-    
+
     let current_year_start = `${start.getFullYear()}-${0+ 1}-${1}`;
     let current_year_end = `${start.getFullYear()}-${11+ 1}-${31}`;
 
     let current_month_start = `${start.getFullYear()}-${start.getMonth() + 1}-${1}`
     let current_month_end = `${start.getFullYear()}-${start.getMonth() + 1}-${31}` ;
-    
-    const commissions = await instance.get("/commissions/stats" ,{params :{
+
+    const commissions_stats = await instance.get("/commissions/stats" ,{params :{
       comm_value_after: current_month_start,
       comm_value_before: current_month_end,
 
@@ -56,14 +82,12 @@ const DashboardContent = () => {
       sale_qty_after: current_month_start,
       sale_qty_before: current_month_end
     }});
-    console.log(commissions.data)
+    console.log(commissions_stats.data)
 
-    // setDataX(commissions.data.map((item: any) => item.id));
-    // setDataY(commissions.data.map((item: any) => item.value));
     
-    setTotalSellsValue(commissions.data.saleValue);
-    setQuantitySellsCurrentMonth(commissions.data.saleQty);
-    setTotalComissionValueCurrentMonth(commissions.data.commValue);
+    setTotalSellsValue(commissions_stats.data.saleValue);
+    setQuantitySellsCurrentMonth(commissions_stats.data.saleQty);
+    setTotalComissionValueCurrentMonth(commissions_stats.data.commValue);
     console.log(totalComissionValueCurrentMonth)
   }
   
@@ -71,24 +95,35 @@ const DashboardContent = () => {
     getDataForGraph();
     getOverallData();
   }
+  useEffect(() => {
+    const newKeys = Object.keys(totalSellsPerMonth);
+    const newValues = Object.values(totalSellsPerMonth);
+  
+    const filteredKeys = newKeys.filter((key) => !dataX.includes(key));
+    const filteredValues = newValues.slice(0, filteredKeys.length);
 
+    setDataX([...filteredKeys,...dataX]);
+    setDataY([...filteredValues,...dataY] as never[]);
+    console.log(dataX);
+    console.log(dataY)
+    console.log(totalSellsPerMonth);
+  }, [totalSellsPerMonth]);
 
   /*
-    TODO 
-      - Implement the logic to filter the data 
-      - Filter by date
-      - Admin sees different data
-      - holy moly this is gonna be a massive request.  
+    TODO.  
+      - Get Info for the piechart
+      - Filters for Linechart
+      - Filters for Piechart
+      - new graph or table?
+      - add button to change month in the values on the right
   */
 
   // console.log(data)
-  // console.log(dataX)
-  // console.log(dataY)
+
+
   useEffect(() => {
+    getDataForGraph();
     getData();
-    setInterval(() => {
-      getData();
-    }, 60000);
   }, []);
 
     return (
@@ -96,7 +131,7 @@ const DashboardContent = () => {
         <div className='col-span-3 flex justify-center p-2'>
 
           <Card className='grow flex justify-center'>
-            <ChartTemplate type='bar' id='test1' title='Test 1' dataX={dataX} dataY={dataY} />
+            <ChartTemplate type='line' id='last12m' title='last12m' dataX={dataX} dataY={dataY} colors={['rgb(147 ,51 ,234)']} />
           </Card>
         </div>
         <div className='col-span-2 flex justify-center p-2'>
