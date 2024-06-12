c2c7d0ab44b3abdc3f7f485a17988d254bc36be8
Markos Nunes <105757405+MarkVN2@users.noreply.github.com>
2024-06-12 08:33:27 -0300

    [quickfix]: wrong width size in card container

--- src/modules/form_card/index.tsx
+++ src/modules/form_card/index.tsx
@@ -4,7 +4,7 @@ import '@/app/globals.css'
 
 const FormCard = ({children}: any) => {
   return (
-    <div className="flex justify-center items-center h-screen md:w-screen  pl-10">
+    <div className="flex justify-center items-center h-screen md:w-[calc(100%-3.4rem)]  pl-10">
       <div className="grid w-full max-w-lg h-full  justify-center items-center">
         <Card className=" flex justify-center items-center border-2 border-gray-300 rounded-lg bg-white shadow-lg p-8">
             {children}
